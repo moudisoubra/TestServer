8667c506ebd46cfd0b3dd2e82d1f0a2a5366fa89
moudisoubra <moudisoubra001@gmail.com>
2019-09-08 17:24:50 +0400

    Added Users

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var fs = require("fs");
 var mongoose = require('mongoose');
 var server = restify.createServer();
 
-console.log('Hello Soubra, Server activated');
+console.log('Test server Activated');
 //
-var playerProfile = [];
+var users = [];
+
 var uristring =
     process.env.MONGODB_URI ||
     process.env.MONGOHQ_URL ||
@@ -17,20 +18,16 @@ mongoose.connect(uristring, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    console.log("Connected to Mongoose!!!!! Mabroooook");
+    console.log("Connected to Mongoose!");
 });
 
-var playerProfileMongo = new mongoose.Schema({
-    player_ID: String,
-    player_Name: String,
-    player_Hat_ID: Number,
-    player_Score: Number,
-    r: Number,
-    g: Number,
-    b: Number
+var usersSchema = new mongoose.Schema({
+    user_ID: String,
+    user_Name: String,
+    user_House: String
 });
 
-var Player = mongoose.model('Player', playerProfileMongo);
+var User = mongoose.model('User', usersSchema);
 
 server.get("/Goodnight", function (req, res, next) 
 {
@@ -47,6 +44,7 @@ server.get("/wakeup", function (req, res, next) {
 
     res.send(string.toString());
 });
+
 server.get("/FindPlayer/:playerID", function (req, res, next) {
 
     var playerID = req.params.playerID;
@@ -66,54 +64,41 @@ server.get("/FindPlayer/:playerID", function (req, res, next) {
     });
 });
 
-server.get("/SaveMongoose/:playerID/:playerName/:playerHatID/:playerScore/:r/:g/:b", function (req, res, next) {
+server.get("/AddUser/:userID/:userName/:userHouse", function (req, res, next) {
 
-    var playerID = req.params.playerID;
-    var playerName = req.params.playerName;
-    var playerHatID = req.params.playerHatID;
-    var playerScore = req.params.playerScore;
-    var r = req.params.r;
-    var g = req.params.g;
-    var b = req.params.b;
+    var userID = req.params.userID;
+    var userName = req.params.userName;
+    var userHouse = req.params.userHouse;
 
-    Player.findOne({ "player_ID": playerID }, (err, player) => {
+    User.findOne({ "user_ID": userID }, (err, user) => {
 
-        if (!player) {
+        if (!user) {
 
             console.log("Didnt find");
 
-            var pl = new Player({
-                "player_ID": playerID,
-                "player_Name": playerName,
-                "player_Hat_ID": playerHatID,
-                "player_Score": playerScore,
-                "r": r,
-                "g": g,
-                "b": b
+            var U = new User({
+                "user_ID": userID,
+                "user_Name": userName,
+                "user_House": userHouse
             });
 
-            console.log("Created: " + pl);
-            res.send({ pl });
+            console.log("Created: " + U);
+            res.send({ U });
 
-            pl.save(function (err) { if (err) console.log('Error on save!') });
+            U.save(function (err) { if (err) console.log('Error on save!') });
         }
         else 
         {
 
-            player = new Player({
+            user = new User({
 
-                "player_ID": playerID,
-                "player_Name": playerName,
-                "player_Hat_ID": playerHatID,
-                "player_Score": playerScore,
-                "r": r,
-                "g": g,
-                "b": b
-
+                "user_ID": userID,
+                "user_Name": userName,
+                "user_House": userHouse
             });
 
-            console.log("Found player: " + player);
-            res.send({ player });
+            console.log("User Found: " + user);
+            res.send({ user });
 
         }
 
@@ -122,244 +107,102 @@ server.get("/SaveMongoose/:playerID/:playerName/:playerHatID/:playerScore/:r/:g/
 
 });
 
-server.get("/ChangeColor/:playerID/:r/:g/:b", function (req, res, next)
-{
-    var playerID = req.params.playerID;
-    var r = req.params.r;
-    var g = req.params.g;
-    var b = req.params.b;
+// server.get("/ChangeColor/:playerID/:r/:g/:b", function (req, res, next)
+// {
+//     var playerID = req.params.playerID;
+//     var r = req.params.r;
+//     var g = req.params.g;
+//     var b = req.params.b;
 
-    Player.findOne({ "player_ID": playerID }, (err, player) => {
+//     Player.findOne({ "player_ID": playerID }, (err, player) => {
 
-        if (!player) {
-            console.log("Didnt find a player with that ID");
-        }
-        else {
-            console.log("Found player: " + player);
+//         if (!player) {
+//             console.log("Didnt find a player with that ID");
+//         }
+//         else {
+//             console.log("Found player: " + player);
 
-            player.r = r;
-            player.g = g;
-            player.b = b;
+//             player.r = r;
+//             player.g = g;
+//             player.b = b;
 
-            player.save(function (err) { if (err) console.log('Error on save!') });
+//             player.save(function (err) { if (err) console.log('Error on save!') });
 
-            res.send({ player });
-        }
-    });
-});
+//             res.send({ player });
+//         }
+//     });
+// });
 
-server.get("/ChangePlayerName/:playerID/:playerName", function (req, res, next)
-{
-    var playerID = req.params.playerID;
-    var playerName = req.params.playerName;
+// server.get("/leaderboard", function (req, res) {
 
-    Player.findOne({ "player_ID": playerID }, (err, player) => {
-        if (!player)
-        {
-            console.log("Didnt find a player with that ID");
-        }
-        else {
-            console.log("Found player: " + player);
-            player.player_Name = playerName;
-            player.save(function (err) { if (err) console.log('Error on save!') });
-            res.send({ player });
-        }
-    });
+//     Player.find({}).sort({ player_Score: -1 }).limit(10).exec(function (err, scores) 
+//     {
+//         var leaderboard = scores;
 
-});
+//         res.send({ leaderboard });
+//     });
+// });
 
-server.get("/ChangePlayerScore/:playerID/:playerScore", function (req, res, next)
-{
-    var playerID = req.params.playerID;
-    var playerScore = req.params.playerScore;
+// server.get("/clearOneMongo/:playerID", function (req, res) { 
+//     //Sets the information based on the input from the user
+//     var player_ID = req.params.playerID;
+//     Player.findOneAndDelete({ "player_ID": player_ID }, (err, player) => { //Finds one user
+//         if (!player) { //If we dont find the player within the database
+//             console.log("Player already deleted!");
+//         }
+//         else {
+//             console.log("Found player: " + player);
+//             res.send({ player }); //The player already exists in the database & will be sent to us.
+//         }
+//     });
+// });
 
-    Player.findOne({ "player_ID": playerID }, (err, player) => {
-        if (!player)
-        {
-            console.log("Didnt find a player with that ID");
-        }
-        else {
+// server.get("/ClearAll", function (req, res) { //BIG RED BUTTON
 
-            if (playerScore > player.player_Score)
-            {
-                console.log("Found player: " + player);
-                player.player_Score = playerScore;
-                player.save(function (err) { if (err) console.log('Error on save!') });
-                res.send({ player });
-            }
-            else
-            {
-                var string = ("Player Score is Higher");
-                res.send({ string });
-            }
-        }
-    });
+//     Player.remove({}, function (err) {
+//         console.log('DataBase Wiped')
 
-});
-
-server.get("/leaderboard", function (req, res) {
-
-    Player.find({}).sort({ player_Score: -1 }).limit(10).exec(function (err, scores) 
-    {
-        var leaderboard = scores;
-
-        res.send({ leaderboard });
-    });
-});
-
-server.get("/ChangePlayerHat/:playerID/:playerHatID", function (req, res, next) {
-
-    var playerID = req.params.playerID;
-    var playerHatID = req.params.playerHatID;
-
-    Player.findOne({ "player_ID": playerID }, (err, player) => {
-        if (!player) {
-            console.log("Didnt find a player with that ID");
-        }
-        else {
-            console.log("Found player: " + player);
-            player.player_Hat_ID = playerHatID;
-            player.save(function (err) { if (err) console.log('Error on save!') });
-            res.send({ player });
-        }
-    });
-
-});
-
-server.get("/clearOneMongo/:playerID", function (req, res) { //REMOVES ONE PLAYER FROM THE DATABASE BASED ON ID SCORE
-    //Sets the information based on the input from the user
-    var player_ID = req.params.playerID;
-    Player.findOneAndDelete({ "player_ID": player_ID }, (err, player) => { //Finds one user
-        if (!player) { //If we dont find the player within the database
-            console.log("Player already deleted!");
-        }
-        else {
-            console.log("Found player: " + player);
-            res.send({ player }); //The player already exists in the database & will be sent to us.
-        }
-    });
-});
-
-server.get("/ClearAll", function (req, res) { //BIG RED BUTTON
-
-    Player.remove({}, function (err) {
-        console.log('DataBase Wiped')
-
-        var string = "DataBase Wiped";
+//         var string = "DataBase Wiped";
     
-        res.send(string.toString());
-    });
+//         res.send(string.toString());
+//     });
 
-});
+// });
 
-server.get("/SortByID", function (req, res) {
+// server.get("/SortByID", function (req, res) {
 
-    Player.find({}).sort({ player_ID: 1 }).exec(function (err, ID)
-    {
-        var leaderboard = ID;
+//     Player.find({}).sort({ player_ID: 1 }).exec(function (err, ID)
+//     {
+//         var leaderboard = ID;
 
-        console.log(leaderboard);
+//         console.log(leaderboard);
 
-        res.send({ leaderboard });
-    });
+//         res.send({ leaderboard });
+//     });
 
-});
+// });
 
-server.get("/listAllMongo", function (req, res) { //LISTS ALL PLAYERS IN THE DATABASE
-    Player.find(function (err, player) {
+// server.get("/listAllMongo", function (req, res) { //LISTS ALL PLAYERS IN THE DATABASE
+//     Player.find(function (err, player) {
 
-        if (err) return console.error(err);
+//         if (err) return console.error(err);
 
-        console.log(player);
+//         console.log(player);
 
-        res.send({ player });
-    });
-});
+//         res.send({ player });
+//     });
+// });
 
-server.get("/ListDataBaseCount", function (req, res) {
+// server.get("/ListDataBaseCount", function (req, res) {
 
-    Player.count({}, function (err, c) {
+//     Player.count({}, function (err, c) {
 
-        res.send({ c });
+//         res.send({ c });
 
-        console.log('Count is ' + c);
-    });
+//         console.log('Count is ' + c);
+//     });
 
-});
-
-//--------------------------------------------------------------SAVING TO FILE--------------------------------------------------------------------------//
-
-server.get("/AddPlayerProfile/:playerID/:score/:aesthetic/:r/:g/:b", function (req, res, next) {
-
-    console.log('Add player');
-
-    var obj = {};
-    obj.id = req.params.playerID;
-    obj.score = req.params.score;
-    obj.aesthetic = req.params.aesthetic;
-    obj.r = req.params.r; 
-    obj.g = req.params.g; 
-    obj.b = req.params.b; 
-    playerProfile.push(obj);
-    res.send({playerProfile});
-    next();
-
-});
-
-server.get("/Print", function (req, res, next) {
-
-    console.log('print');
-    res.send({playerProfile});
-    next();
-
-});
-
-server.get("/DeleteArray", function (req, res, next) {
-
-    console.log('print');
-    playerProfile.length = 0;
-    res.send({ playerProfile });
-    next();
-
-});
-
-server.get("/ReadFile", function (req, res, next) {
-
-    fs.readFile("temp.txt", function (err, buf) {
-
-        var string = buf.toString();
-
-        res.send(JSON.parse(string));
-        
-    });
-});
-
-server.get("/SaveFile", function (req, res, next) {
-
-    var data = JSON.stringify(playerProfile);
-
-    fs.writeFile("temp.txt", data, (err) => {
-
-        if (err) console.log(err);
-
-        res.send("Successfully Written to File Manually.");
-        console.log("Successfully Written to File Manually.");
-    });
-});
-
-
-function SavingToFile()
-{
-    var data = JSON.stringify(playerProfile);
-
-    fs.writeFile("temp.txt", data, (err) => {
-
-        if (err) console.log(err);
-
-        console.log("Successfully Written to File.");
-    });
-
-}
+// });
     
 //--------------------------------------------------------------SAVING TO FILE--------------------------------------------------------------------------//
 

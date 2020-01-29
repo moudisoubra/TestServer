fa99b4b4a22ada57cba4c98902f4bce1855ed3f7
moudisoubra <moudisoubra001@gmail.com>
2020-01-29 17:57:22 +0400

    Sending Emailn works

--- emailSender.js
+++ emailSender.js
@@ -1,33 +1,30 @@
-
-
     console.log("emailer up");
     var fs = require('fs');
-var path = require('path');
+    var path = require('path');
 
-var nodeMailer = require('nodemailer');
-let transporter = nodeMailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: 'true',
-    port: 25,
-    auth:{
-        user: 'testdummynodejs@gmail.com',
-        pass: 'nodeJS1234'
-    },
-    tls:{
-        rejectUnauthorized: 'false'
-    }
-});
-let HelperOptions = {
-    from: '"Mohammad Soubra" testdummynodejs@gmail.com',
-    to: 'moudisoubra001@gmail.com',
-    subject: 'please work',
-    text: 'please work more'
-};
-transporter.sendMail(HelperOptions, (error, info) => {
-    if(error){
-        console.log(error);
-    }
-    console.log("Email sent");
-    console.log(info);
-})
+    var nodeMailer = require('nodemailer');
+    let transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: 'true',
+        auth:{
+            user: 'testdummynodejs@gmail.com',
+            pass: 'nodeJS1234'
+        },
+        tls:{
+            rejectUnauthorized: 'false'
+        }
+    });
+    let HelperOptions = {
+        from: '"Form Dummy" testdummynodejs@gmail.com',
+        to: 'moudisoubra001@gmail.com',
+        subject: 'Job Application Form', 
+        text: 'This is another test email'
+    };
+    transporter.sendMail(HelperOptions, (error, info) => {
+        if(error){
+            console.log(error);
+        }
+        console.log("Email sent");
+        console.log(info);
+    })

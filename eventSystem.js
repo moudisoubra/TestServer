4a7ed53b3c5c6ee44d11bf72d243cd9e64965fd1
moudisoubra <moudisoubra001@gmail.com>
2020-02-02 14:45:50 +0400

    Event System Finished

--- eventSystem.js
+++ eventSystem.js
@@ -6,7 +6,7 @@ function EventSystem(events, mongoose)
     //                   Voted Against
     //                   Number of people Voted
     //                   List of people Voted
-    //Whenever Someone votes they getted added to relative list of voted for or against and list of people voted
+    //Whenever Someone votes they get added to relative list of voted for or against and list of people voted
     //To check if someone has voted before check if they are in that list of people voted
     //If someone has voted, the event will still appear on their screen but it will appear wihtout voting allowed
 
@@ -16,12 +16,17 @@ function EventSystem(events, mongoose)
         totalNumberVoted: Number,
         numberVotedAgainst: Number,
         numberVotedFor: Number,
-        likePercentage: Number
+        eventID: String
     });
 
     var eventModel = mongoose.model('event', eventSchema);
 
     console.log("Event System Started");
+
+    events.get("/eventPage", function(req, res)
+    {
+        res.sendFile(__dirname+"/eventPage.html");
+    });
 
     events.get("/CreateEvent/:eventName/:eventDescription", function(req, res)
     {
@@ -32,12 +37,32 @@ function EventSystem(events, mongoose)
         var newEvent = new eventModel({
 
             "eventName": eventName,
-            "eventDescription": eventDescription
+            "eventDescription": eventDescription,
+            "totalNumberVoted": 0,
+            "numberVotedAgainst": 0,
+            "numberVotedFor": 0
         });
-        
+        newEvent.awardID = newEvent._id;
         newEvent.save(function (err) { if (err) console.log('Error on save!') });
 
         res.send({newEvent});
+    });
+
+    events.post("/WebsiteCreateEvent", function(req, res)
+    {
+        var eventName = req.body.eventName; 
+        var eventDescription = req.body.eventDescription;
+
+
+        var newEvent = new eventModel({
+
+            "eventName": eventName,
+            "eventDescription": eventDescription
+        });
+        newEvent.eventID = newEvent._id;
+        newEvent.save(function (err) { if (err) console.log('Error on save!') });
+
+        res.sendFile(__dirname+"/EventPosted.html");
     });
 
     events.get("/ListEvents", function (req, res) { //LISTS ALL Teams IN THE DATABASE
@@ -52,23 +77,23 @@ function EventSystem(events, mongoose)
         });
     });
 
-    events.get("/AddToPositive/:eventName/:number", function (req, res) { 
+    events.get("/AddToPositive/:eventID/:number", function (req, res) { 
         
-        var eventName = req.params.eventName;
+        var eventID = req.params.eventID;
         var points = req.params.number;
         
-        eventModel.findOne({ "eventName": eventName }, (err, event) => {
+        eventModel.findOne({ "eventID": eventID }, (err, event) => {
             if (!event) {
                 console.log("Didnt find an event with that name");
                 res.send("Didnt find an event with that name");
             }
             else {
 
-                    eventModel.updateOne({ eventName: eventName },{$inc: { numberVotedFor: points } }, function (error, result) {
+                    eventModel.updateOne({ eventID: eventID },{$inc: { numberVotedFor: points } }, function (error, result) {
                         if (error) res.send(error);
                         else res.send(result);
                     });
-                    eventModel.updateOne({ eventName: eventName },{$inc: { totalNumberVoted: points } }, function (error, result) {
+                    eventModel.updateOne({ eventID: eventID },{$inc: { totalNumberVoted: points } }, function (error, result) {
                         if (error) res.send(error);
                         else res.send(result);
                     });
@@ -77,34 +102,42 @@ function EventSystem(events, mongoose)
 
     });
 
-    events.get("/AddToNegative/:eventName/:number", function (req, res) { 
+    events.get("/AddToNegative/:eventID/:number", function (req, res) { 
         
-        var eventName = req.params.eventName;
+        var eventID = req.params.eventID;
         var points = req.params.number;
         
-        eventModel.findOne({ "eventName": eventName }, (err, event) => {
+        eventModel.findOne({ "eventID": eventID }, (err, event) => {
             if (!event) {
                 console.log("Didnt find an event with that name");
                 res.send("Didnt find an event with that name");
             }
             else {
 
-                    eventModel.updateOne({ eventName: eventName },{$inc: { numberVotedAgainst: points } }, function (error, result) {
+                    eventModel.updateOne({ eventID: eventID },{$inc: { numberVotedAgainst: points } }, function (error, result) {
                         if (error) res.send(error);
                         else res.send(result);
                     });
-                    eventModel.updateOne({ eventName: eventName },{$inc: { totalNumberVoted: points } }, function (error, result) {
+                    eventModel.updateOne({ eventID: eventID },{$inc: { totalNumberVoted: points } }, function (error, result) {
                         if (error) res.send(error);
                         else res.send(result);
                     });
             }
         });
 
+    });    
+    
+    events.get("/ClearAllEvents", function (req, res) { //BIG RED BUTTON
+
+        eventModel.remove({}, function (err) {
+            console.log('Events DataBase Wiped')
+    
+            var string = "Events DataBase Wiped";
+        
+            res.send(string.toString());
+        });
+    
     });
 
-    events.get("/FindPercentage", function (req, res) {
-      
-
-    });
 }
 module.exports = EventSystem;
